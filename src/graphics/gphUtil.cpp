2ccf5b1ddb81717a7141543395b138be35279824
NikitaPishel <pogerproger@gmail.com>
2025-06-01 21:33:30 +0100

    feat: add Colors class

--- src/graphics/gphUtil.cpp
+++ src/graphics/gphUtil.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <unordered_map>
 #include "gphUtil.h"
 
 // graphics utility namespace
@@ -14,4 +15,50 @@ namespace gph {
 
         return window;
     };
+
+    const Colors& Colors::getInstance() {
+        static Colors instance;
+        return instance;
+    }
+
+    // constructor, inits color map
+    Colors::Colors()
+        : colorMap{
+            {"none", -1},
+            {"black", 0},
+            {"red", 1},
+            {"green", 2},
+            {"yellow", 3},
+            {"blue", 4},
+            {"magenta", 5},
+            {"cyan", 6},
+            {"white", 7},
+            {"b_black", 8},
+            {"b_red", 9},
+            {"b_green", 10},
+            {"b_yellow", 11},
+            {"b_blue", 12},
+            {"b_magenta", 13},
+            {"b_cyan", 14},
+            {"b_white", 15}
+        }
+        // empty constructor body
+    {
+
+    }
+
+    // returns color id, if color not found return black and warn
+    int Colors::getColorID(std::string color) const {
+        auto colorRecord = colorMap.find(color);
+
+        if (colorRecord != this->colorMap.end()) {
+            return colorRecord->second;
+        }
+
+        else {
+            std::cerr << "Error: invalid color" << color << "\n" << std::endl;
+            return 0;
+        }
+    }
+
 };
--- src/graphics/gphUtil.h
+++ src/graphics/gphUtil.h
@@ -1,9 +1,26 @@
 #ifndef CANV_GPH_UTIL
 #define CANV_GPH_UTIL
+#include <string>
 #include <sys/ioctl.h>
+#include <unordered_map>
 
 namespace gph {
     winsize getWinSize();
+
+    class Colors {
+    public:
+        static const Colors& getInstance();
+
+        int getColorID(std::string color) const;
+
+    private:
+        Colors();
+
+        std::unordered_map<std::string, int> colorMap;
+
+        Colors(const Colors&) = delete;
+        Colors& operator=(const Colors&) = delete;
+    };
 };
 
 #endif
