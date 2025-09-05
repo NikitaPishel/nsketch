f7ab291696f9dd4dcf59beed480a60cecc55e5be
NikitaPishel <pogerproger@gmail.com>
2025-09-05 18:36:48 +0100

    feat: add build script

---
+++ devBuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+# Variables
+BUILD_DIR="./build"
+SOURCE_DIR="."
+BUILD_TESTS="${TESTS:-ON}"
+AUTO_TESTS="${AUTO_TESTS:-OFF}"
+
+# Create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Configure CMake and build the project
+cmake -B "$BUILD_DIR" -S "$SOURCE_DIR" -DBUILD_TESTS=$BUILD_TESTS
+cmake --build "$BUILD_DIR"
+
+# Run tests automatically if AUTO_TESTS is true
+if [ "$AUTO_TESTS" = "ON" ] && [ "$BUILD_TESTS" = "ON" ]; then
+    cd "$BUILD_DIR"
+    ctest --output-on-failure
+fi
