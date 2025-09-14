525b559aa26b79873d958922b0d63f821d5d35fd
NikitaPishel <pogerproger@gmail.com>
2025-09-14 16:31:04 +0100

    feat: test render

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <chrono>
+#include "nsketch/gph/canvas.h"
+#include "nsketch/gph/texture.h"
+
 using namespace std;
+using namespace gph;
 
 int main() {
-    std::cout << "Hello World\n";
+    Canvas canv(32, 12);
+    canv.updateSize();
+
+    Texture texWhite = Texture::Builder(2, 1)
+        .fillTexture(' ', "white", "white")
+        .build();
+
+    Texture texBlack = Texture::Builder(2, 1)
+        .fillTexture(' ', "white", "black")
+        .build();
+
+    // build chessboard pattern
+    for (int j = 0; j < canv.getYSize(); j++) {
+        for (int i = 0; i < canv.getXSize() / 2; i++) {
+            bool isWhite = ((i + j) % 2 == 0);
+            const auto& tex = isWhite ? texWhite : texBlack;
+            canv.addTexture(i * 2, j, tex);
+        }
+    }
+
+    constexpr int numRenders = 50000;
+    double totalTime = 0.0;
+
+    for (int f = 0; f < numRenders; f++) {
+        auto start = chrono::high_resolution_clock::now();
+
+        canv.render();
+
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = end - start;
+        totalTime += elapsed.count();
+    }
+
+    double avgFrameTime = totalTime / numRenders;
+    double avgFPS = 1.0 / avgFrameTime;
+
+    std::cout << "Average FPS over " << numRenders << " renders: " << avgFPS << "\n";
+
     return 0;
 }
