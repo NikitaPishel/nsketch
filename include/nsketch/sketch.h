855567ea0b23ae61fec3b790bbfb201a0d0c3923
NikitaPishel <pogerproger@gmail.com>
2025-09-23 00:23:03 +0100

    feat: add matrix as a separate internal class

--- include/nsketch/sketch.h
+++ include/nsketch/sketch.h
@@ -1,18 +1,22 @@
 #include <string>
 #include <cstdint>
 #include <vector>
+#include <memory>
 #include "cursor.h"
 
 namespace nsk {
     class Sketch {
     private:
-        std::vector<std::string> sketch;
+        // hidden matrix implementation
+        class Impl; 
+        std::unique_ptr<Impl> pImpl;
 
         uint16_t xSize;
         uint16_t ySize;
         uint16_t sketchSize;
 
     public:
+        // Create a cursor of a sketch
         Cursor cursor;
     
         // Constructor
---
+++ src/sketch/matrix.h
@@ -0,0 +1,41 @@
+#ifndef CANV_GRID_H
+#define CANV_GRID_H
+#include <vector>
+#include <string>
+#include <cstdint>
+
+namespace nsk {
+    // matrix holder class with basic matrix control
+    class Matrix {
+        
+    public:
+        // matrix dimensions to exclude constant vector size recall
+        uint32_t xSize;
+        uint32_t ySize;
+        
+        // Basic control methods
+        Matrix(int xSize = 0, int ySize = 0);
+        void setMatrixSize(int xSize, int ySize);
+        
+        void setPixel(int xPos, int yPos, char symbol = ' ', std::string textColor = "0", std::string backColor = "0");
+        void addPixel(int xPos, int yPos, std::string color);
+        
+        std::string& getPixel(int xPos, int yPos);
+        const std::string& getPixel(int xPos, int yPos) const;
+        
+        std::string& getPixelByIndex(int index);
+        const std::string& getPixelByIndex(int index) const;
+        
+        // get x and y position of a pixel with index x
+        const std::pair<uint32_t, uint32_t> getPixelPos(int index) const;
+        
+        // total matrix size. Used for internal functionality (indexing)
+        uint32_t matrixSize;
+        
+    private:
+        // matrix itself; Vector that works as a projector of a linear indexed matrix
+        std::vector<std::string> matrix;
+    };
+}
+
+#endif
