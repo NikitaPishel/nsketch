b1227fa934025190b2073c3a7dee91401b974489
NikitaPishel <pogerproger@gmail.com>
2025-09-23 21:30:21 +0100

    feat: implement sketch class

--- include/nsketch/sketch.h
+++ include/nsketch/sketch.h
@@ -2,34 +2,35 @@
 #include <cstdint>
 #include <vector>
 #include <memory>
-#include "cursor.h"
 
 namespace nsk {
     class Sketch {
     private:
-        // hidden matrix implementation
-        class Impl; 
-        std::unique_ptr<Impl> pImpl;
+        // Matrix of pixels, pImpl
+        class MatrixImpl; 
+        std::unique_ptr<MatrixImpl> mImpl;
 
         uint16_t xSize;
         uint16_t ySize;
         uint16_t sketchSize;
 
     public:
-        // Create a cursor of a sketch
-        Cursor cursor;
+        // Create a cursor of a sketch, pImpl
+        class CursorImpl;
+        std::unique_ptr<CursorImpl> cImpl;
     
         // Constructor
-        Sketch(int xSize, int ySize);    
+        Sketch(int xSize = 1, int ySize = 1);
 
         // Size getters
-        const int getXSize() const;
-        const int getYSize() const;
+        int getXSize() const;
+        int getYSize() const;
 
         // Change sketch size
         void setSketchSize(int xSize, int ySize);
         
         // Draw methods
         void fillZone();
+        void draw();
     };
 }
--- src/sketch/sketch.cpp
+++ src/sketch/sketch.cpp
@@ -1,3 +1,56 @@
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <memory>
+#include "nsketch/sketch.h"
+#include "matrix.h"
+#include "cursor.h"
+
 namespace nsk {
+    // Define internal implementation of matrix
+    class Sketch::MatrixImpl {
+        public:
+            Matrix matrix;
+            
+            MatrixImpl(int xSize, int ySize) : matrix(xSize, ySize) {};
+    };
     
+    // Define internal implementation of cursor
+    class Sketch::CursorImpl {
+        public:
+            Cursor cursor;
+
+            CursorImpl() : cursor() {};
+    };
+
+    // Constructor
+    Sketch::Sketch(int xSize, int ySize) :
+        mImpl(std::make_unique<MatrixImpl>(xSize, ySize)),
+        cImpl(std::make_unique<CursorImpl>())
+        {}
+
+    // Get x size of a drawing
+    int Sketch::getXSize() const {
+        return this->mImpl->matrix.xSize;
+    }
+
+    // Get y size of a drawing
+    int Sketch::getYSize() const {
+        return this->mImpl->matrix.xSize;
+    }
+
+    // Change the size of the drawing
+    void Sketch::setSketchSize(int xSize, int ySize) {
+        this->mImpl->matrix.setMatrixSize(xSize, ySize);
+    }
+
+    // Fill up the selected zone (if slctZone is on)
+    void Sketch::fillZone() {
+
+    }
+
+    // Draw 1 pixel on cursor
+    void Sketch::draw() {
+
+    }
 }
