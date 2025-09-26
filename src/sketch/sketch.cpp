fcb8293916ccfc8212aadf614844f02eb7c12956
NikitaPishel <pogerproger@gmail.com>
2025-09-26 14:22:03 +0100

    feat: save color ids instead of color names in sketch

--- src/sketch/sketch.cpp
+++ src/sketch/sketch.cpp
@@ -3,9 +3,12 @@
 #include <vector>
 #include <memory>
 #include <ngph/texture.h>
+#include <ngph/colors.h>
 #include "nsketch/sketch.h"
-#include "matrix.h"
 #include "nsketch/cursor.h"
+#include "matrix.h"
+
+using namespace gph;
 
 namespace nsk {
     // Define internal implementation of matrix
@@ -37,28 +40,39 @@ namespace nsk {
 
     // Add a single pixel to a matrix by coordinates
     void Sketch::addPixel(int xPos, int yPos, std::string color) {
-        this->mImpl->matrix.setPixel(xPos, yPos, color);
+        const Colors& gphColors = Colors::getInstance();
+        std::string colorId = gphColors.getColorId(color);
+        
+        this->mImpl->matrix.setPixel(xPos, yPos, colorId);
     };
     
     // Add a single pixel to a matrix by relative index
     void Sketch::addPixelByIndex(int index, std::string color) {
-        this->mImpl->matrix.setPixelByIndex(index, color);
+        const Colors& gphColors = Colors::getInstance();
+        std::string colorId = gphColors.getColorId(color);
+        
+        this->mImpl->matrix.setPixelByIndex(index, colorId);
     };
 
     // Fill up the selected zone (if slctZone is on)
     void Sketch::fillZone(int xPos, int yPos, int xSize, int ySize, std::string color) {
+        const Colors& gphColors = Colors::getInstance();
+        std::string colorId = gphColors.getColorId(color);
+
         for (uint16_t xShift = 0; xShift < xSize; xShift++) {
             for (uint16_t yShift = 0; yShift < ySize; yShift++) {
                 uint16_t pixXPos = xPos + xShift;
                 uint16_t pixYPos = yPos + yShift;
                 
-                this->mImpl->matrix.setPixel(pixXPos, pixYPos, "black");
+                this->mImpl->matrix.setPixel(pixXPos, pixYPos, colorId);
             }
         }
     }
 
     // Will be used to display a sketch later
-    gph::Texture Sketch::texturize() {
-        
+    Texture Sketch::texturize() {
+        for (uint16_t i; i < this->mImpl->matrix.matrixSize; i++) {
+
+        }
     }
 }
