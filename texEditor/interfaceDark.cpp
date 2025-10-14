3aa5f4da22f5466d15322d76cf5d8f05d0dfbae2
NikitaPishel <pogerproger@gmail.com>
2025-10-14 02:20:56 +0100

    feat: update new texture packs to light and dark

---
+++ texEditor/interfaceDark.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <ngph/texture.h>
+#include <ngph/iotex.h>
+
+using namespace std;
+using namespace gph;
+
+int main() {
+    string path = "./data/textures/tex-base-dark.gph";
+    
+    // init interface texture table
+    TexTable uiTexPack;
+    uiTexPack.loadTable(path);
+
+    // workspace background
+    Texture workspBack = Texture::Builder(1, 1)
+        .setPixel(0, 0, ' ', "white", "black")
+        .build();
+
+    // single pixel top menu
+    Texture menuTopInner = Texture::Builder(1, 1)
+        .setPixel(0, 0, ' ', "white", "black")
+        .build();
+        
+    // top menu border
+    Texture menuTopBorder = Texture::Builder(1, 1)
+        .setPixel(0, 0, '-', "white", "black")
+        .build();
+    
+    // single pixel tab
+    Texture menuTopTabInner = Texture::Builder(1, 1)
+        .setPixel(0, 0, ' ', "white", "black")
+        .build();
+    
+    // top menu border
+    Texture menuTopTabBorder = Texture::Builder(1, 1)
+        .setPixel(0, 0, '-', "white", "black")
+        .build();
+    
+    // single pixel down menu
+    Texture menuDownInner = Texture::Builder(1, 1)
+        .setPixel(0, 0, ' ', "white", "yellow")
+        .build();
+    
+    // down menu border
+    Texture menuDownBorder = Texture::Builder(1, 1)
+        .setPixel(0, 0, '-', "white", "black")
+        .build();
+    
+    // nSketch label on the bottom
+    Texture menuDownLabel = Texture::Builder(7, 1)
+        .setPixel(0, 0, 'n', "black", "yellow")
+        .setPixel(1, 0, 'S', "black", "yellow")
+        .setPixel(2, 0, 'k', "black", "yellow")
+        .setPixel(3, 0, 'e', "black", "yellow")
+        .setPixel(4, 0, 't', "black", "yellow")
+        .setPixel(5, 0, 'c', "black", "yellow")
+        .setPixel(6, 0, 'h', "black", "yellow")
+        .build();
+    
+    // left menu border
+    Texture menuLeftBorder = Texture::Builder(1, 1)
+        .setPixel(0, 0, '|', "white", "black")
+        .build();
+    
+    // left menu fill
+    Texture menuLeftInner = Texture::Builder(1, 1)
+        .setPixel(0, 0, ' ', "white", "black")
+        .build();
+    
+    uiTexPack.setTexture("workspace.background", workspBack);
+    uiTexPack.setTexture("menuTop.inner", menuTopInner);
+    uiTexPack.setTexture("menuTop.border", menuTopBorder);
+    uiTexPack.setTexture("menuTop.tab.inner", menuTopTabInner);
+    uiTexPack.setTexture("menuTop.tab.border", menuTopTabBorder);
+    uiTexPack.setTexture("menuDown.inner", menuDownInner);
+    uiTexPack.setTexture("menuDown.Border", menuDownBorder);
+    uiTexPack.setTexture("menuDown.label", menuDownLabel);
+    uiTexPack.setTexture("menuLeft.border", menuLeftBorder);
+    uiTexPack.setTexture("menuLeft.inner", menuLeftInner);
+
+    uiTexPack.saveTable(path);
+    
+    return 0;
+}
